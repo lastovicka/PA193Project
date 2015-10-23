7ed3e3f1f63eabc922a8acb073a77266ed54d2f0
lastovicka <lastovickam@gmail.com>
2015-10-23 15:30:28 +0200

    Hello world

---
+++ PA193Project/PA193Project.cpp
@@ -0,0 +1,14 @@
+// PA193Project.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello World!";
+    return 0;
+}
+
