af240cfa8a7e93ea412e6212c3d86f500de988c3
lastovicka <lastovickam@gmail.com>
2015-11-09 22:07:19 +0100

    Fix of parser automaton

--- PA193Project/PA193Project.cpp
+++ PA193Project/PA193Project.cpp
@@ -1,4 +1,5 @@
 // PA193Project.cpp : Defines the entry point for the console application.
+// author Martin Lastovicka
 //
 
 #include "stdafx.h"
@@ -14,12 +15,40 @@
 
 using namespace std;
 
-int main()
-{
-	ifstream inStream("example.htm");
+//no stack implementation, use this for testing
+bool push(string element){return true;}
+string pop(){return "";}
+string top(){return "";}
+bool isEmpty(){return true;}
+
+
+/*
+* Function to verify if element is a "void element", see http://www.w3.org/html/wg/drafts/html/master/syntax.html#elements-2
+*
+* return true   if element is void
+*        false  if element is not void
+*/
+bool isVoidElement(string element) {
+	string voidElements[] = { "area", "base", "br", "col", "embed", "hr", "img", "input", "keygen", "link",
+		"menuitem", "meta", "param", "source", "track", "wbr" };
+	for (int i = 0; i < 16; i++) {
+		if (element.compare(voidElements[i]) == 0) {
+			return true;
+		}
+	}
+	return false;
+}
+
+int main(int argc, char * argv[]){
+	if (argc != 2) {
+		cerr << "You need to specify file to open" << "\n";
+		return 0;
+	}
+	ifstream inStream(argv[1]);
 	if (!inStream.is_open())
 	{
 		cerr << "ERROR: Unable to open file!" << "\n";
+		return 0;
 	}
 
 	bool invalidInput = false;
@@ -28,6 +57,7 @@ int main()
 	string element = "";
 	string attribute = "";
 	int state = START;
+
 	while (inStream.get(current))
 	{
 		if (!inStream.good()) {
@@ -42,6 +72,7 @@ int main()
 			if (current != '<' && current != ' ' && current != '\t' && current != '\n' && current != '\f' && current != '\r' && current != '\v') {
 				// only whitespaces allowed before first element
 				invalidInput = true;
+				break;
 			}
 			if (current == '<'){
 				state = INSIDE_TAG;
@@ -50,6 +81,10 @@ int main()
 		case INSIDE_TAG:
 			if (current == '!') {
 				state = INSIDE_SPECIAL_TAG;
+				break;
+			}
+			if (current == '/') {
+				state = END_ELEMENT;
 				break;
 			}
 			element += current;
@@ -63,12 +98,12 @@ int main()
 			}
 			if (current == ' ') {
 				// DOCTYPE is the only special element allowed
-				if (element.compare("DOCTYPE") != 0) {
+				for (unsigned int i = 0; i < element.length(); i++) element[i] = (char)tolower(element[i]);
+				if (element.compare("doctype") != 0) {
 					cerr << "ERROR: invalid special element!" << "\n";
 					invalidInput = true;
 					break;
-				}
-				else {
+				} else {
 					if (!addElement(element)) {
 						cerr << "ERROR: cannot add element to list of elements!" << "\n";
 						invalidInput = true;
@@ -82,12 +117,14 @@ int main()
 			element += current;
 			break;
 		case DROP_SPECIAL_TAG:
+			// we dont validate special tags attributes
 			if (current == '>') {
 				state = INSIDE_ELEMENT;
 			}
 			break;
 		case INSIDE_COMMENT:
 			if (current == '-' && previous == '-') {
+				// only "-->" is valid closing sequence of a HTML comment
 				inStream.get(current);
 				if (!inStream.good()) {
 					cerr << "ERROR: an error occured while reading file!" << "\n";
@@ -105,12 +142,22 @@ int main()
 			// do nothing, we dont care what is inside comments
 			break;
 		case PROCESS_ELEMENT:
+			//element name could end with " " (has attribute), "/" (self closing tag) or ">" (end of tag)
 			if (current == ' ') {
-				if (!push(element)) {
-					cerr << "ERROR: cannot push element to stack!" << "\n";
+				for (unsigned int i = 0; i < element.length(); i++) element[i] = (char) tolower(element[i]);
+				if (!isValidElement(element)) {
+					cerr << "ERROR: invalid element \"" << element << "\"" << "\n";
 					invalidInput = true;
 					break;
 				}
+				// "void elements" in HTML5 cannot be closed by closing tags
+				if (!isVoidElement(element)) {
+					if (!push(element)) {
+						cerr << "ERROR: cannot push element to stack!" << "\n";
+						invalidInput = true;
+						break;
+					}
+				}				
 				if(!addElement(element)) {
 					cerr << "ERROR: cannot add element to list of elements!" << "\n";
 					invalidInput = true;
@@ -118,10 +165,16 @@ int main()
 				}
 				element.clear();
 				state = PROCESS_ATTRIBUTE;
+				break;
 			}
 			if (current == '/') {
+				for (unsigned int i = 0; i < element.length(); i++) element[i] = (char)tolower(element[i]);
+				if (!isValidElement(element)) {
+					cerr << "ERROR: invalid element \"" << element << "\"" << "\n";
+					invalidInput = true;
+					break;
+				}
 				// dont push element on stack, it closed itself
-
 				if (!addElement(element)) {
 					cerr << "ERROR: cannot add element to list of elements!" << "\n";
 					invalidInput = true;
@@ -140,12 +193,22 @@ int main()
 				}
 				element.clear();
 				state = INSIDE_ELEMENT;
+				break;
 			}
 			if (current == '>') {
-				if (!push(element)) {
-					cerr << "ERROR: cannot push element to stack!" << "\n";
+				for (unsigned int i = 0; i < element.length(); i++) element[i] = (char)tolower(element[i]);
+				if (!isValidElement(element)) {
+					cerr << "ERROR: invalid element \"" << element << "\"" << "\n";
 					invalidInput = true;
 					break;
+				}
+				// "void elements" in HTML5 cannot be closed by closing tags
+				if (!isVoidElement(element)) {
+					if (!push(element)) {
+						cerr << "ERROR: cannot push element to stack!" << "\n";
+						invalidInput = true;
+						break;
+					}
 				}
 				if (!addElement(element)) {
 					cerr << "ERROR: cannot add element to list of elements!" << "\n";
@@ -154,11 +217,13 @@ int main()
 				}
 				element.clear();
 				state = INSIDE_ELEMENT;
+				break;
 			}
 			element += current;
 			break;
 		case PROCESS_ATTRIBUTE:
-			if (current == '=') {
+			if (current == '=' || current == ' ' || current == '>' || current == '/') {
+				for (unsigned int i = 0; i < attribute.length(); i++) attribute[i] = (char)tolower(attribute[i]);
 				if (!isValidAttribute(attribute)) {
 					cerr << "ERROR: invalid attribute \"" << attribute << "\"" << "\n";
 					invalidInput = true;
@@ -167,8 +232,8 @@ int main()
 				string currentElement = top();
 				if (currentElement.empty()) {
 					cerr << "ERROR: stack is empty while processing attribute" << "\n";
-					invalidInput = true;
-					break;
+					//invalidInput = true;
+					//break;
 				}
 				if (!isValidPair(currentElement, attribute)) {
 					cerr << "ERROR: invalid pair of element and attribute: \"" << element << "\" - \"" << attribute << "\"" << "\n";
@@ -176,11 +241,16 @@ int main()
 					break;
 				}
 				attribute.clear();
-				state = DROP_ATTRIBUTE_VALUE;
+				if (current == '=') { state = DROP_ATTRIBUTE_VALUE; }
+				// some attributes does not have to have value
+				if (current == ' ') { state = PROCESS_ATTRIBUTE; }
+				if (current == '>') { state = INSIDE_ELEMENT; }
+				if (current == '/') { state = NEXT_ATTRIBUTE; }
 				break;
 			}
-			if (current == ' ' || current == '\t' || current == '\n' || current == '\f' || current == '\r' || current == '\v') {
-				// whitespaces allowed
+			if (attribute.compare("") == 0 && (current == ' ' || current == '\t' || current == '\n' || current == '\f' 
+												|| current == '\r' || current == '\v')) {
+				// whitespaces allowed before attribute
 				break;
 			}
 			attribute += current;
@@ -192,7 +262,32 @@ int main()
 			// do nothing, we dont care what is element content
 			break;
 		case DROP_ATTRIBUTE_VALUE:
-			if (current == '"' && previous != '=') {
+			if (current == '\"' && previous == '=') {
+				state = DROP_ATTRIBUTE_VALUE_ESCAPED;
+			}
+			if (current == ' ') {
+				state = PROCESS_ATTRIBUTE;
+			}
+			if (current == '>') {
+				state = INSIDE_ELEMENT;
+			}
+			if (current == '/') {
+				inStream.get(current);
+				if (!inStream.good()) {
+					cerr << "ERROR: an error occured while reading file!" << "\n";
+					invalidInput = true;
+					break;
+				}
+				if (current != '>') {
+					cerr << "ERROR: invalid closing of tag with attribute" << "\n";
+					invalidInput = true;
+					break;
+				}
+				state = INSIDE_ELEMENT;
+			}
+			break;
+		case DROP_ATTRIBUTE_VALUE_ESCAPED:
+			if (current == '"' && previous != '\\') {
 				state = NEXT_ATTRIBUTE;
 			}
 			break;
@@ -205,34 +300,37 @@ int main()
 					break;
 				}
 				if (current != '>') {
-					cerr << "ERROR: invalid closing of comment" << "\n";
+					cerr << "ERROR: invalid closing of tag with attribute" << "\n";
 					invalidInput = true;
 					break;
 				}
 				state = INSIDE_ELEMENT;
+				break;
 			}
 			if (current == '>') {
 				state = INSIDE_ELEMENT;
+				break;
 			}
-			if (current == ' ' || current == '\t' || current == '\n' || current == '\f' || current == '\r' || current == '\v') {
-				state = PROCESS_ATTRIBUTE;
-			}
+			// this state just handles unexpected tag end after attribute
+			state = PROCESS_ATTRIBUTE;
 			break;
 		case END_ELEMENT:
 			if (current == '>') {
+				for (unsigned int i = 0; i < element.length(); i++) element[i] = (char)tolower(element[i]);
 				if (isEmpty()) {
 					cerr << "ERROR: Too many element closings!" << "\n";
-					invalidInput = true;
-					break;
+					//invalidInput = true;
+					//break;
 				}
 				if (element.compare(top()) != 0) {
-					cerr << "ERROR: invalid closing of element " << top() << "\n";
-					invalidInput = true;
-					break;
+					cerr << "ERROR: invalid closing of element \"" << top() << "\", current closing is for \"" << element << "\"\n";
+					//invalidInput = true;
+					//break;
 				}
 				element.clear();
 				pop();
 				state = INSIDE_ELEMENT;
+				break;
 			}
 			element += current;
 			break;
@@ -243,6 +341,12 @@ int main()
 		}
 
 		previous = current;
+
+		if (element.length() > 11) {
+			cerr << "ERROR: invalid element occured!" << "\n";
+			invalidInput = true;
+			break;
+		}
 
 		if (invalidInput) {
 			cerr << "ERROR: Validation of HTML file failed!" << "\n";
@@ -274,4 +378,5 @@ int main()
 
 	return 0;
 }
+
 
